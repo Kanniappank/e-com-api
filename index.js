ffb75e0d7a75604cf11d0602de9a25f43d21e41f
Kanniappan K <kkumaresan@thapovan-inc.com>
2024-07-31 15:51:33 +0530

    cors issue fixed

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ const cors = require('cors');
 const { logger } = require('./middlewares/logEvents');
 const errorHandler = require('./middlewares/errorLogger');
 const PORT = process.env.PORT || 8000;
-const whitelist = ["http://localhost:8000","http://127.0.0.1:5500","www.google.com","https://www.google.com"]
-corsOption = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) != -1) {
-            callback(null, true)
-        }
-        else {
-            callback(new Error('Not Allowed by cors'))
-        }
-    },
-    optionsSuccessStatus:200,
-}
+const whitelist = ["http://localhost:8000", "http://127.0.0.1:5500", "www.google.com", "https://www.google.com"]
+const corsOptions = {
+    origin: whitelist, // Allow only this origin
+    methods: 'GET,POST,PATCH,DELETE', // Allow only GET and POST requests
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
 
-app.use(cors(corsOption));
+app.use(cors(corsOptions));
 
 app.use(logger);
 
@@ -27,7 +22,6 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-
     res.send('Hi peps');
 })
 
