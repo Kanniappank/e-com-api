87e211071ca74b43514d4de97d9d73cb72e87038
Kanniappan <suriyastart007@gmail.com>
2024-09-09 23:31:13 +0530

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cors = require('cors');
 const { logger } = require('./middlewares/logEvents');
 const errorHandler = require('./middlewares/errorLogger');
+const rootRouter = require('./routes/root')
 const PORT = process.env.PORT || 8000;
 const whitelist = ["http://localhost:8000", "http://127.0.0.1:5500", "www.google.com", "https://www.google.com"]
 const corsOptions = {
@@ -11,7 +12,7 @@ const corsOptions = {
     methods: 'GET,POST,PATCH,DELETE', // Allow only GET and POST requests
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
+};
 
 app.use(cors(corsOptions));
 
@@ -21,16 +22,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Hi peps');
-})
+app.use('/',rootRouter  )
 
-app.use(errorHandler);
 
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`))
